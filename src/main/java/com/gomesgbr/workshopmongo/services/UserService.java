100fb30a78a3e809b5579b368ad85f7fc3303795
Gabriel Almeida <gabrielg299@gmail.com>
2022-09-28 13:10:31 -0300

    Inserção de usuário com POST

--- src/main/java/com/gomesgbr/workshopmongo/services/UserService.java
+++ src/main/java/com/gomesgbr/workshopmongo/services/UserService.java
@@ -1,12 +1,15 @@
 package com.gomesgbr.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gomesgbr.workshopmongo.domain.User;
+import com.gomesgbr.workshopmongo.dto.UserDTO;
 import com.gomesgbr.workshopmongo.repository.UserRepository;
+import com.gomesgbr.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +19,21 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> user =  repo.findById(id);
+        
+        
+
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO){
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
