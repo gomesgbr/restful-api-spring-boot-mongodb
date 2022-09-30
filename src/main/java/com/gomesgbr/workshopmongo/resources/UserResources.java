875381e5348dd53592ba1c98d2b45ab4e6d03da4
Gabriel Almeida <gabrielg299@gmail.com>
2022-09-30 10:38:07 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/gomesgbr/workshopmongo/resources/UserResources.java
+++ src/main/java/com/gomesgbr/workshopmongo/resources/UserResources.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -52,4 +51,14 @@ public class UserResources {
 
        
     }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    
 }
