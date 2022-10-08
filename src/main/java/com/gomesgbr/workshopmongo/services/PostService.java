a47d7525576f0908a59a820d7500b2dd39449372
Gabriel Almeida <gabrielg299@gmail.com>
2022-10-08 18:31:57 -0300

    Obtendo um post por id

---
+++ src/main/java/com/gomesgbr/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.gomesgbr.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gomesgbr.workshopmongo.domain.Post;
+import com.gomesgbr.workshopmongo.repository.PostRepository;
+import com.gomesgbr.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id){
+        Optional<Post> user =  repo.findById(id);
+        
+        
+
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
