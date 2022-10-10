e64244c374880fe2660f0dcad23cf09db51c1a91
Gabriel Almeida <gabrielg299@gmail.com>
2022-10-10 12:17:12 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/gomesgbr/workshopmongo/domain/Post.java
+++ src/main/java/com/gomesgbr/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.gomesgbr.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.gomesgbr.workshopmongo.dto.AuthorDTO;
+import com.gomesgbr.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -18,6 +21,8 @@ public class Post implements Serializable{
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post(){
 
@@ -77,6 +82,18 @@ public class Post implements Serializable{
 
     public void setBody(String body) {
         this.body = body;
+    }
+
+
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
 
